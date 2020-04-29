d932c8edd718bb22b7b146d94a5ba5a39bf6dce2
SofianeLounis <sofiane.lounis@etu.univ-amu.fr>
2020-04-29 17:24:51 +0200

    J'ai effectuer des changements.

--- src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
+++ src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
@@ -2,6 +2,11 @@ package fr.univ_amu.iut.exercice1;
 
 public class HelloWorld {
     public static String hello(String s) {
-        throw new RuntimeException("Not yet implemented !");
+
+        if (s.isEmpty()) {
+            return "Hello, World!";
+        }
+
+
+        return "";
     }
-}
